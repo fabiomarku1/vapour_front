f32038cfed34dfb3c5961f4760c8a92d2e1406f4
Fabio Marku <111870245+fabiomarku1@users.noreply.github.com>
2023-02-16 16:31:28 +0100

    chenges

--- login/login.js
+++ login/login.js
@@ -1,74 +1,78 @@
-'use strict'
-var email = document.getElementById("username");
-var password = document.getElementById("password");
 
-var TOKEN = "";
+let email = document.getElementById("username");
+let password = document.getElementById("password");
+
+ var _UserId="";
+ var _TOKEN = "";
+//  export {_UserId};
+
+
 const url = "http://localhost:80/testPhp/Auth/login";
 
 
 async function login() {
-    return await fetch(url, {
-        method: 'POST',
-        body: JSON.stringify({ "Email": `${email.value}`, "Password": `${password.value}` }),
-    })
-        .then((e) => e.json())
-        .then((json) => json);
+  return await fetch(url, {
+    method: 'POST',
+    body: JSON.stringify({ "Email": `${email.value}`, "Password": `${password.value}` }),
+  })
+    .then((e) => e.json())
+    .then((json) => json);
 }
 
-function HanldeError( message)
-{
-    document.getElementById("sign-Error").innerHTML=message;
-    document.getElementById("sign-Error").style.display="block";
-    return;
+function HanldeError(message) {
+  document.getElementById("sign-Error").innerHTML = message;
+  document.getElementById("sign-Error").style.display = "block";
+  return;
 }
 
 function Validate(email, password) {
-    if (!email || email.trim() === '' || !password || password.trim() === '') {
-      return false;
-    }
- 
-    /* // Check if email is valid
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(email)) {
-      return false;
-    }
-  
-    // Check if password meets minimum requirements
-    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
-    if (!passwordRegex.test(password)) {
-      return false;
-    }
-  */
-    return true;
+  if (!email || email.trim() === '' || !password || password.trim() === '') {
+    return false;
   }
-addEventListener("submit", (e) => {
+
+  /* // Check if email is valid
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+    return false;
+  }
+ 
+  // Check if password meets minimum requirements
+  const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+  if (!passwordRegex.test(password)) {
+    return false;
+  }
+*/
+  return true;
+}
+addEventListener("submit", async (e) => {
   e.preventDefault();
 
-    if(!Validate(email.value,password.value))
-    {
-      HanldeError("Not valid email");
-      return;
-    }
+  if (!Validate(email.value, password.value)) {
+    HanldeError("Not valid email");
+    return;
+  }
 
-    login().then(request => 
-    {
-        if(request["code"]==400)
-            HanldeError("Password is wrong")
+  const request = await login();
 
-        if(request["code"]==404)
-        HanldeError("No email were found!");
 
-        if(request["token"]!=null)
-        {
-            TOKEN = request["token"];
-            console.log(TOKEN);
-             window.location.replace('../home/home.html');
-        }
-    })
-    console.log(TOKEN);
+  if (request["code"] == 400)
+    HanldeError("Password is wrong")
 
+  if (request["code"] == 404)
+    HanldeError("No email were found!");
+
+  if (request["token"] != null) 
+  {
+    _TOKEN = request["token"];
+    let decodedData = jwt_decode(_TOKEN)
+    _UserId = decodedData["Id"];
+
+    window.localStorage.setItem("token",_TOKEN);
+    window.localStorage.setItem("userId",_UserId);
    
-    
+     window.location.replace('../home/home.html');
 
-
+  }
 })
+
+
